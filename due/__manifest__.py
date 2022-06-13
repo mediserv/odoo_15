5efa8908da8cca3e59590613585e39dac3b8a99b
mediserv <89163735+mediserv@users.noreply.github.com>
2022-06-13 07:28:04 -0400

    Add files via upload

---
+++ due/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#    
+#
+###################################################################################
+
+{
+    'name': 'Coverage Period Followup',
+    'version': '14.0.1.0.0',
+    'author': 'Centralasis Proactive Solutions',
+    'maintainer': 'Centralasis Proactive Solutions',
+    'company': 'Centralasis Proactive Solutions',
+    'website': 'https://www.centralasis.com',
+    'depends': ['account', 'mail', 'sms', 'account_reports', 'base', 'hr_payroll', 'sale_management', 'sale_subscription',
+                ],
+    'data': [ 	'security/ir.model.access.csv',
+    		    'views/res_partner_cus.xml',
+                'wizard/auxillary_package.xml',
+                'wizard/upgrade_downgrade.xml',
+                'views/due_subscription_form.xml',
+            	'views/due_sale_order_form.xml',
+            ],
+    'qweb': [
+        "static/src/xml/account_reconciliation.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
